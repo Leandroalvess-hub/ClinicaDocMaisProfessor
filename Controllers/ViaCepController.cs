3b903d75962cb36f671fec092f75552d2b63fae7
giovannirlmartins <giovannirlmartins@gmail.com>
2026-04-16 15:35:20 -0300

    Agendamento de consulta com sucesso

--- Controllers/ViaCepController.cs
+++ Controllers/ViaCepController.cs
@@ -1,11 +1,49 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ClinicaDocMais.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ClinicaDocMais.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class ViaCepController : ControllerBase
     {
+        [HttpGet("{cep}")]
+        public async Task<IActionResult> BuscaEndereco(string cep)
+        {
+            if (cep.Length != 8)
+            {
+                return NotFound("Cep deve conter 8 dígitos");
+            }
+            try
+            {
+                var client = new HttpClient();
+                var response = await client.GetAsync($"https://viacep.com.br/ws/{cep}/json/");
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadFromJsonAsync<ViaCepModel>();
+                    if (content.cep == null)
+                    {
+                        return NotFound("Cep não encontrado.");
+                    }
+
+                    return Ok(content);
+                }
+                else
+                {
+                    return NotFound("CEP não encontrado.");
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine("Erro no serviço: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro Inesperado: " + ex.Message);
+            }
+            return BadRequest();
+
+        }
     }
 }
